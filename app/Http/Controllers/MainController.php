9ab723bc8fa63b14de42b64a4d93c6bf28eda51d
SnorlaxSPI <aflinsjr@icloud.com>
2025-06-28 19:36:17 -0300

    Blade - introdução

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,36 +9,7 @@ class MainController extends Controller
 {
     public function showView(): View
     {
-        // método 1
-        /*
-        $data = [
-            'name' => 'Alessandro Lins',
-            'description' => 'This is the main page of our application.',
-        ];
 
-        return view('admin.newPage3', $data);
-        */
-        // método 2
-        /*
-        return view('admin.newPage3', [
-            'name' => 'Alessandro Lins',
-            'description' => 'This is the main page of our application.',
-        ]);
-        */
-
-        // método 3
-        /*
-        $name = 'Alessandro Lins';
-        $description = 'This is the main page of our application.';
-        return view('admin.newPage3')->with(compact('name', 'description'));
-        */
-
-        // método 4
-        $name = 'Alessandro Lins';
-        $description = 'This is the main page of our application.';
-        return view('admin.newPage3')->with([
-            'name' => $name,
-            'description' => $description,
-        ]);
+        return view('home', ['name' => 'John Doe']);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts/main_layout')
 @section('content')
-    <p class = "display-1 text-center">Hello World</p>
-    </div>
+    {{-- comentário de blade --}}
+    <p>{{ $name }}</p>
+    <p>{{ $description }}</p>
 @endsection
